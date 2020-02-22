44bb0e3a434757d66dbfac21da421f2cdf7a4ae1
Danny Rehelis <autogun@gmail.com>
2020-02-22 14:50:30 +0200

    Fix crond to actually pick the file
    crond default path reads file based on user existent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,12 @@ export FLASK_APP=web.py
 export FLASK_ENV=development
 
 CRON_PATH="/var/spool/cron/crontabs"
-CRON_FILE="$CRON_PATH/sammy_ofer"
+CRON_FILE="$CRON_PATH/root"
 
-echo "0 8 * * * python $(readlink -f cron.py)" > $CRON_FILE
+echo "0 9 * * * python $(readlink -f cron.py)" >> $CRON_FILE
 chmod 0600 "$CRON_FILE"
 
-crond -l 8 -d 8 -L /var/log/crond -c $CRON_PATH
+crond -L /var/log/crond -c $CRON_PATH
 
 flask run --host=0.0.0.0 --port=5000
 
