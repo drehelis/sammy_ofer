4c731c05d4f3def4ccab1a340d3827be72e18f57
Danny Rehelis <autogun@gmail.com>
2022-07-18 17:51:59 +0300

    6

--- cron.py
+++ cron.py
@@ -40,6 +40,7 @@ def checkForGamesToday(games):
         if today == value[0].date():
             logging.info("Yesh mishak!")
             yield value
+
     return False
 
 def createMessage(*args):
@@ -58,7 +59,7 @@ def createMessage(*args):
         custom_road_block_time = f"החל מ-{road_block_time}"
         if int(specs_number) >= 28000:
             custom_sepcs_number = f"({specs_number:,}) 😱"
-        if specs_word == "ללא" or int(specs_number) =< 6000:
+        if specs_word == "ללא" or int(specs_number) <= 6000:
             custom_road_block_time = "אין"
         elif specs_word == "גדול מאוד":
             custom_road_block_time = f"החל מ-{(datetime.datetime.strptime(road_block_time,'%H:%M') - datetime.timedelta(hours=1)).strftime('%H:%M')}"
