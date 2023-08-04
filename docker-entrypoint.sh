f4d0ec2eb5a8ac84f3a8a459a7b5fc5d2213671f
Danny Rehelis <autogun@gmail.com>
2023-08-04 09:21:54 +0300

    Fix entrypoint & today date

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,12 @@ export FLASK_ENV=development
 
 CRON_PATH="/var/spool/cron/crontabs"
 CRON_FILE="$CRON_PATH/root"
+CRON_EXPR="0 6 * * *" # UTC
 
-echo "0 9 * * * python $(readlink -f cron.py)" >> $CRON_FILE
-chmod 0600 "$CRON_FILE"
+if ! grep -q "cron.py" $CRON_FILE; then
+  echo "$CRON_EXPR python $(readlink -f cron.py)" >> $CRON_FILE
+  chmod 0600 "$CRON_FILE"
+fi
 
 crond -L /var/log/crond -c $CRON_PATH
 
