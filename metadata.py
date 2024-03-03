be504a52380fbad13432dc49c597e70012217880
Danny Rehelis <autogun@gmail.com>
2024-03-03 07:06:45 +0200

    Mar, 3 Updates

---
+++ metadata.py
@@ -0,0 +1,53 @@
+TEAMS_METADATA = {
+    "מכבי חיפה": {
+        "name": "Maccabi Haifa",
+        "url": "https://www.football.co.il/maccabi-haifa",
+    },
+    "הפועל חדרה": {
+        "name": "Hapoel Hadera",
+        "url": "https://www.football.co.il/hapoel-hadera",
+    },
+    "הפועל חיפה": {
+        "name": "Hapoel Haifa",
+        "url": "https://www.football.co.il/hapoel-haifa",
+    },
+    "מכבי פתח תקווה": {
+        "name": "Maccabi Petah Tikva",
+        "url": "https://www.football.co.il/maccabi-petah-tikva",
+    },
+    "הפועל ב\"ש": {
+        "name": "Hapoel Beer Sheva",
+        "url": "https://www.football.co.il/hapoel-beer-sheva",
+    }
+}
+
+EMOJI_HEARTS = [
+    '💖',
+    '💞',
+    '💚',
+    '💜',
+    '💓',
+    '💙',
+    '💘',
+    '🤍',
+    '💗',
+    '💕',
+    '💛',
+    '🧡',
+    '💝',
+    '🤎',
+    '❤️',
+    '❤️‍🔥',
+    '💟',
+    '❣️',
+    '🖤'
+]
+
+POLL_SENTENCES = [
+    'סקר: מי תנצח?',
+    'סקר: מי תנצח במפגש בין שתי הקבוצות?',
+    'סקר: מי תיקח הפעם?',
+    'סקר: מי האחת שתנצח ותפתח ברגל ימין?',
+    'סקר: מי הקבוצה הטובה יותר?',
+    'סקר: אילו אוהדים יחגגו היום?'
+]
