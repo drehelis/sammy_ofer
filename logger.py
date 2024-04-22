f74a1ebb9c65acc8e72f64d69eac2b8258aa6649
Danny Rehelis <autogun@gmail.com>
2024-04-22 14:20:14 +0300

    static content only

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="{'time':'%(asctime)s', 'level': '%(levelname)s', 'message': '%(message)s'}",
-    handlers=[logging.StreamHandler()]
-)
-
-logger = logging.getLogger(__name__)
